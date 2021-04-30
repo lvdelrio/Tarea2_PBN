3a701aebaa06fdf53df343578b1eb576f10dc2f3
AgBallivian <agballivian@miuandes.cl>
2021-04-30 19:51:37 -0400

    Free memory ahora

--- Delrio-Ballivian-Tarea2.c
+++ Delrio-Ballivian-Tarea2.c
@@ -6,7 +6,7 @@
 //struct generos
 
 int numero_lineas(FILE*file){ //Contador de Lineas del archivo
-    char ch;
+    char ch;                  //Nos podemos ahorrar esto con la wea de feof en original creo
     int lineas = 0;
     while(!feof(file)){
         ch = fgetc(file);
@@ -20,9 +20,33 @@ int numero_lineas(FILE*file){ //Contador de Lineas del archivo
     rewind(file); //Reicinio el Puntero de fgets para usearlo otra vez
     return lineas;
 }
+/*
+int run_archivo(FILE*file){
+    for(int c=0; c<numerodedatos; c++){ //Tratar de recorrer 10 nombres
+        printf("numero de iteracion %d \n", c);
+        char** linea = malloc(3*sizeof(char*));
+        for (int i=0;i<5;i++){
+            linea[i]=malloc(100*sizeof(char));
+            }  
+        fgets(line,sizeof line,genres);
+        printf("%s",line);
 
-
-
+        char* token =strtok(line, ";");
+        
+        while(token !=NULL){
+            char* palabra= malloc(100*sizeof(char));
+            strcpy(palabra,token);
+            //strcpy(linea[contador],palabra);
+            linea[contador]=palabra;
+            printf("%s\n",palabra);
+            token = strtok(NULL,";");
+            contador++;
+            free(palabra);
+        }
+    free(linea);
+    }
+} 
+*/
 
 
 
@@ -40,11 +64,12 @@ int main(){
     printf("%d Datos\n", numerodedatos);
 
     
-    char line[255]; //Hay que declarar eso diferente por las decimas
+    char *line;
     int contador = 0;
     char*** lineas = malloc(sizeof(char**));
     lineas[0]=malloc(3*sizeof(char*));
     int cont_lines = 0;
+
 //------------inicio de programa------------
     int decision=1;
     while(decision){
@@ -53,14 +78,15 @@ int main(){
         scanf("%d",&decision);
         
         if(decision==1){
-            for(int c=0; c<numerodedatos; c++){ //Tratar de recorrer 10 nombres
-                printf("numero de iteracion %d \n", c);
+            for(int c=0; c<10; c++){ //Tratar de recorrer 10 nombres
                 char** linea = malloc(3*sizeof(char*));
                 for (int i=0;i<5;i++){
                     linea[i]=malloc(100*sizeof(char));
                     }  
+                
+
                 fgets(line,sizeof line,genres);
-                printf("%s",line);
+                printf("DATO : %s ",line);
 
                 char* token =strtok(line, ";");
 
@@ -82,6 +108,9 @@ int main(){
         }
         
     }
+    free(lineas[0]);
+    free(lineas);
+    //free(line); //este no se libera pq no es malloc (Creo?).
     return 0;
 }
             
