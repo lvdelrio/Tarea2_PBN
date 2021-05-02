96aeabee5fce15a4d032794084ef03f5d130b355
lvdelrio <lvdelrio@miuandes.cl>
2021-05-02 01:41:00 -0400

    guarda los datos del primer txt

--- Delrio-Ballivian-Tarea2.c
+++ Delrio-Ballivian-Tarea2.c
@@ -3,7 +3,14 @@
 #include <stdlib.h>
 
 
-//struct generos
+struct generos{
+    char* genero; 
+    char* id; 
+    char* artista; 
+    int popu;
+    char* major; 
+    
+};
 
 int numero_lineas(FILE*file){ //Contador de Lineas del archivo
     char ch;
@@ -38,6 +45,8 @@ int main(){
     genres=fopen("genres.txt","r");
     songs=fopen("songs.txt","r");
 
+    struct generos* datos = malloc(sizeof(struct generos));
+
 
     int numerodedatos = numero_lineas(genres); //Cuento lineas
     printf("%d Datos\n", numerodedatos);
@@ -59,11 +68,13 @@ int main(){
         scanf("%d",&decision);
         
         if(decision==1){
+            
             for(int c=0; c<10; c++){ //Tratar de recorrer 10 nombres
                 char** linea = malloc(3*sizeof(char*));
                 for (int i=0;i<5;i++){
                     linea[i]=malloc(100*sizeof(char));
-                    }  
+                    } 
+                 
                 fgets(line,256,genres);
                 printf("DATO : %s",line);
                 
@@ -74,18 +85,52 @@ int main(){
                     strcpy(palabra,token);
                     //strcpy(linea[contador],palabra);
                     linea[contador]=palabra;
-                    printf("%s\n",palabra);
+                    if(contador==0){
+                        datos[c].genero=linea[contador];
+                        }
+                    if(contador==1){
+                        datos[c].id=linea[contador];
+                        }
+                    if(contador==2){
+                        datos[c].artista=linea[contador];
+                        }
+                    printf("%s",linea[0]);
                     token = strtok(NULL,";");
                     contador++;
-                    free(palabra);
-                }
+                    
+                    
+                }//termina el while
+                datos = realloc(datos,(c+2)*(sizeof(struct generos)));
+                
+                if(cont_lines>=1){
+                    int aux_count=cont_lines+1;
+                    char*** aux_lineas = realloc(lineas,aux_count*sizeof(char**));
+                    lineas=aux_lineas;
+                    lineas[cont_lines]=malloc(3*sizeof(char*));
+                    }
+                lineas[cont_lines]=linea;
+                printf("%s %s %s",lineas[0][0],lineas[0][1],lineas[0][2]);
+                contador = 0;
+                cont_lines++;
+                
             
             free(linea);
             printf("Dato c : %d \n ", c);
-            }
+            }//termina el for
+        }//termina el if
+        printf("esto es contador %d",cont_lines);
+        for(int i=0;i<cont_lines-1;i++){
+            printf("\n");
+            printf("%s ",datos[i].genero);
+            printf("%s ",datos[i].id);
+            printf("%s ",datos[i].artista);
+            printf("%d ",datos[i].popu);
+            printf("%s \n",datos[i].major);
+            
         }
+        printf("hola bune señior");
         
-    }
+    }//termina el while
     free(lineas[0]);
     free(lineas);
     free(line);
